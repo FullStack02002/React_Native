00bc1673359978628949cc9ae99ddbe2e26f1c55
Gaj Singh Rathore <gajrathore353@gmail.com>
2026-04-02 20:49:41 +0530

    updated expo router ---updated layout.tsx file

--- expo_router/app/_layout.tsx
+++ expo_router/app/_layout.tsx
@@ -1,5 +1,17 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router"
+import { Text, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>My Expo App</Text>
+      </View>
+      <Slot/>
+      <View>
+        <Text>Gaj Singh</Text>
+      </View>
+    </SafeAreaView>
+  )
 }
